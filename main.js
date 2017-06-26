fc16984d681cd0d8c08e90110ece09afcc6b3be9
shnarch1 <shnarch@gmail.com>
2017-06-27 00:33:27 +0300

    inital header design

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ fetch("api/playlist").then(function(response){return response.json()})
 					 		playlist.build();
 					 	}
 					 })
+
+
+// function curvedText(text, radius){
+// 	var text = text.split("");
+// 	var deg_offset = 100 / text.length;
+// 	var deg_origin = 0;
+// 	var class_index = document.getElementsByClassName("curved-header")[0];
+// 	console.dir(class_index);
+
+// 	text.forEach((ea) => {
+//     	ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${deg_origin}deg);transform-origin:0 100%'>${ea}</p>`;
+//     	class_index.innerHTML += ea;
+//     	deg_origin += deg_offset;
+//   });
+// }
+// curvedText("test", 110)
