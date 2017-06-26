64caf775d3b7827e8e378557011ec4b55086d382
shnarch1 <shnarch@gmail.com>
2017-06-26 22:18:08 +0300

    added basice playlist class

---
+++ playlist.js
@@ -0,0 +1,22 @@
+class Playlist {
+	
+	constructor(_id, name, img_url, container_name){
+		this._id = _id;
+		this.name = name;
+		this.img_url = img_url;
+		this.container_name = container_name;
+	}
+
+	build(){
+		var pl_container = $("<div>", {class:"plasylist-container",
+									   "data-id":this._id});
+		var pl_header = $("<div>", {class:"curved-header",
+									text:this.name})
+									.appendTo(pl_container);
+		var pl_img_container = $("<div>", {class:"pl-img-container"})
+									.appendTo(pl_container);
+		var pl_img = $("<img>", {src:this.img_url}).appendTo(pl_img_container);
+		var pl_circle = $("<div>", {class:"circle"}).appendTo(pl_img_container);
+		pl_container.appendTo(this.container_name);
+	}	
+}
