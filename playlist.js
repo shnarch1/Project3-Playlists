6afcdeaa6ae262d42f715377024367b54f20ffa6
shnarch1 <shnarch@gmail.com>
2017-07-07 15:59:11 +0300

    Added playlist buttons css design

--- playlist.js
+++ playlist.js
@@ -15,12 +15,31 @@ class Playlist {
 		var pl_img_container = $("<div>", {class:"pl-img-container"})
 									.appendTo(pl_container);
 		var pl_img = $("<img>", {src:this.img_url}).appendTo(pl_img_container);
+
+		var del_edit_btns_container = $("<div>", {class: "del-edit-btns-container"})
+									.appendTo(pl_img_container);
+
+		var del_btn = $("<div>", {class: "del-btn"})
+									.append($("<button>")
+										.append($("<sapn>", {class: "glyphicon glyphicon-remove"})))
+									.appendTo(del_edit_btns_container);
+
+		var edit_btn = $("<div>", {class: "edit-btn"})
+									.append($("<button>")
+										.append($("<sapn>", {class: "glyphicon glyphicon-pencil"})))
+									.appendTo(del_edit_btns_container);
+
 		var pl_circle = $("<div>", {class:"circle"}).appendTo(pl_img_container);
+
+		var play_btn = $("<button>", {class: "play-pl"})
+									.append($("<span>", {class: "glyphicon glyphicon-play"}))
+									.appendTo(pl_circle);
+		
 		pl_container.appendTo(container_name);
 	}
 
 	static buildAll(){
-		$('#main-container').empty();
+		// $('#main-container').empty();
 		fetch("api/playlist").then((response)=>{return response.json()})
 				 .then(function(data){
 				 	for(var i=0; i<data.data.length; i++){
